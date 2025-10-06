1b4cdb74ff12b1750b8b2e30dce4773034b3e9a9
keygen_exe <matkovskiyf3vlad@gmail.com>
2025-10-06 03:20:10 +0300

    refactor(DEV-14): fixed image path

--- src/api/iconsMap.ts
+++ src/api/iconsMap.ts
@@ -1,17 +1,17 @@
-import IconGithub from '../img/icons/Github.svg?react';
-import IconBitcoin from '../img/icons/Bitcoin.svg?react';
-import IconCalendar from '../img/icons/Calendar.svg?react';
-import IconCard from '../img/icons/Card.svg?react';
-import IconDiscord from '../img/icons/Discord.svg?react';
-import IconDribble from '../img/icons/Dribble.svg?react';
-import IconEmail from '../img/icons/Email.svg?react';
-import IconFigma from '../img/icons/Figma.svg?react';
-import IconLandmark from '../img/icons/Landmark.svg?react';
-import IconLinkedin from '../img/icons/Linkedin.svg?react';
-import IconPlay from '../img/icons/Play.svg?react';
-import IconQuotes from '../img/icons/quotes.svg?react';
-import IconTelegram from '../img/icons/Telegram.svg?react';
-import IconTwitter from '../img/icons/Twitter.svg?react';
+import IconGithub from '/img/icons/Github.svg?react';
+import IconBitcoin from '/img/icons/Bitcoin.svg?react';
+import IconCalendar from '/img/icons/Calendar.svg?react';
+import IconCard from '/img/icons/Card.svg?react';
+import IconDiscord from '/img/icons/Discord.svg?react';
+import IconDribble from '/img/icons/Dribble.svg?react';
+import IconEmail from '/img/icons/Email.svg?react';
+import IconFigma from '/img/icons/Figma.svg?react';
+import IconLandmark from '/img/icons/Landmark.svg?react';
+import IconLinkedin from '/img/icons/Linkedin.svg?react';
+import IconPlay from '/img/icons/Play.svg?react';
+import IconQuotes from '/img/icons/quotes.svg?react';
+import IconTelegram from '/img/icons/Telegram.svg?react';
+import IconTwitter from '/img/icons/Twitter.svg?react';
 
 export const iconsMap = {
   calendar: IconCalendar,
